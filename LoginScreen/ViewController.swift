768bb17e63d074172184bbcab22848ba5c039b46
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-24 11:46:50 +0300

    setup project

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - View's life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
     }
-
-
 }
 
