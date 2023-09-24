a806107ca12549e173b7e75f262fa0ab3c1ecd0a
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-24 14:54:22 +0300

    lesson 2 done

--- LoginScreen/ViewControllers/AuthViewController.swift
+++ LoginScreen/ViewControllers/AuthViewController.swift
@@ -27,6 +27,9 @@ final class AuthViewController: UIViewController {
         titleColor: .black
     )
     
+    private let emailStack = UIStackView()
+    private let loginStack = UIStackView()
+    
     // MARK: - View's life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +45,8 @@ private extension AuthViewController {
         addSubviews()
         
         setupLogo()
+        setupEmailStack()
+        setupLoginStack()
         
         setupLayout()
     }
@@ -51,7 +56,9 @@ private extension AuthViewController {
 private extension AuthViewController {
     func addSubviews() {
         [
-            logoImage
+            logoImage,
+            loginStack,
+            emailStack
         ].forEach { subView in
             view.addSubview(subView)
         }
@@ -61,13 +68,41 @@ private extension AuthViewController {
         logoImage.image = UIImage(named: "logo")
         logoImage.contentMode = .scaleAspectFit
     }
+    
+    func setupEmailStack() {
+        emailStack.axis = .vertical
+        
+        [
+            emailLabel,
+            emailButton
+        ].forEach { subView in
+            emailStack.addArrangedSubview(subView)
+        }
+    }
+    
+    func setupLoginStack() {
+        loginStack.axis = .vertical
+        
+        [
+            loginLabel,
+            loginButton
+        ].forEach { subView in
+            loginStack.addArrangedSubview(subView)
+        }
+    }
 }
 
 // MARK: - Layout
 private extension AuthViewController {
     func setupLayout() {
         [
-            logoImage
+            logoImage,
+            loginLabel,
+            loginButton,
+            loginStack,
+            emailLabel,
+            emailButton,
+            emailStack
         ].forEach { subView in
             subView.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -76,7 +111,19 @@ private extension AuthViewController {
             logoImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
-            logoImage.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25)
+            logoImage.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
+            
+            emailStack.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 30),
+            emailStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            emailStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            emailStack.heightAnchor.constraint(equalToConstant: 150),
+            emailButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            loginStack.topAnchor.constraint(equalTo: emailStack.bottomAnchor, constant: 30),
+            loginStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            loginStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            loginStack.heightAnchor.constraint(equalToConstant: 150),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
